ce704d8728ff331a1697c2934f272bdabbec0424
rohhit <rohhit@rohhit-Inspiron-5558>
2020-02-15 11:03:44 +0530

    adding new program

---
+++ Java_language/src/git/Pattern_Program.java
@@ -0,0 +1,40 @@
+package git;
+
+public class Pattern_Program 
+{
+	void input()
+	{
+		System.out.println("enter the no. of line");
+		Object sc = nextInt();
+		
+	}
+	private Object nextInt() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static void main(String[] args)
+	{
+		
+		
+		int i; int j;
+		for(i=1; i<=4; i++)
+		{
+			for(j=1; j<=i; j++)
+			{ 
+				System.out.print("*");
+		    }
+		System.out.println();
+	    }
+	}	
+
+}
+
+
+
+ 
+
+	
+	
+
+
+
