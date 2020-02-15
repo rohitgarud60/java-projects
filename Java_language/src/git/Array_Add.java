2158f2c884c708e78c8500809d418206bfd7fd8a
rohhit <rohhit@rohhit-Inspiron-5558>
2020-02-15 11:19:46 +0530

    Second program

---
+++ Java_language/src/git/Array_Add.java
@@ -0,0 +1,18 @@
+package git;
+
+import java.util.Scanner;
+
+public class Array_Add
+{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the first number");
+		a= sc.nextInt();
+		System.out.println("enter the second number");
+		b=sc.nextInt();
+		c= a-b;
+		System.out.println("Mul is-"+c);
+	}
+}
